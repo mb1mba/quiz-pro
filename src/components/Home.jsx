8d243c118a3f215806d120dae7d5281498c7cc6b
Theo Kimbangui Mbemba <126973795+mb1mba@users.noreply.github.com>
2023-07-23 18:07:51 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Home(props){
+export default function Home({toggleState){
     return(
         <div className="home-page">
             <h1 className="home-page-text">What?</h1>
-            <button onClick={props.toggleState} className="button home">Start quiz</button>
+            <button onClick={toggleState} className="button home">Start quiz</button>
         </div>
     )
 }
